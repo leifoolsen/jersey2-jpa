2459617803888444938a75d153b919bd4433c1a6
Leif Olsen <leif.o.olsen@gmail.com>
2015-04-05 14:17:13 +0200

    Code refacor

--- src/main/java/com/github/leifoolsen/jerseyjpa/util/StringUtil.java
+++ src/main/java/com/github/leifoolsen/jerseyjpa/util/StringUtil.java
@@ -3,7 +3,8 @@ package com.github.leifoolsen.jerseyjpa.util;
 import com.google.common.base.MoreObjects;
 
 public class StringUtil {
-    public StringUtil() {}
+
+    private StringUtil() {}
 
     public static String blankToNull(final String value) {
         String s = MoreObjects.firstNonNull(value, "").trim();
